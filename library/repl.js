caaf6a3f09a4a082140253d532448906495e7e19
emkot <flyerccount41@gmail.com>
2025-09-04 13:24:11 -0400

    Create repl.js

---
+++ library/repl.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+const { EclipseRuntime } = require('./library/MainC.JS');
+require('./library/ExtraC.JS');
+require('./library/OtherC.JS');
+require('./library/SN.JS');
+
+const e = new EclipseRuntime();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Eclipse> '
+});
+
+rl.prompt();
+
+rl.on('line', async (line) => {
+    try {
+        await e.process(line); // interpret your Eclipse code dynamically
+    } catch(err) {
+        console.error('Error:', err);
+    }
+    rl.prompt();
+}).on('close', () => {
+    console.log('Bye!');
+    process.exit(0);
+});
